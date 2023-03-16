2db88799f5d4ac4bcc044b4901db28d6c56fa34b
alitrk <turkali750@gmail.com>
2023-03-16 15:25:11 +0300

    NetworkBoundResource.kt file was created and implemented in logic

--- app/src/main/java/com/codinginflow/simplecachingexample/features/restaurants/RestaurantViewModel.kt
+++ app/src/main/java/com/codinginflow/simplecachingexample/features/restaurants/RestaurantViewModel.kt
@@ -1,11 +1,9 @@
 package com.codinginflow.simplecachingexample.features.restaurants
 
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.codinginflow.simplecachingexample.api.RestaurantApi
 import com.codinginflow.simplecachingexample.data.Restaurant
+import com.codinginflow.simplecachingexample.data.RestaurantRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -13,10 +11,12 @@ import javax.inject.Inject
 
 @HiltViewModel
 class RestaurantViewModel @Inject constructor(
-    api:RestaurantApi
+    repository: RestaurantRepository
 ): ViewModel() {
 
-    private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
+    val restaurants = repository.getRestaurants().asLiveData()
+
+    /*private val restaurantsLiveData = MutableLiveData<List<Restaurant>>()
     val restaurants: LiveData<List<Restaurant>> = restaurantsLiveData
 
     init {
@@ -25,5 +25,5 @@ class RestaurantViewModel @Inject constructor(
             delay(2000)
             restaurantsLiveData.value = restaurants
         }
-    }
+    }*/
 }
